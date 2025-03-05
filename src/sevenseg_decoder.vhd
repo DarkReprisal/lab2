48276d58de57133926864d6eac2a3dfc392d02f1
DarkReprisal <178818154+DarkReprisal@users.noreply.github.com>
2025-03-05 13:19:10 -0700

    edit2

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -55,6 +55,7 @@ begin
                    "0100111" when  "1100",
                    "0100001" when  "1101",
                    "0000110" when  "1110",
-                   "0001110" when  "1111";
+                   "0001110" when  "1111",
+                   "1000000" when others;
 
 end Behavioral;
